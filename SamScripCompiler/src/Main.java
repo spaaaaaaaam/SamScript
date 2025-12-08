9e1a2a133553eaa55c9f2a7246a7816429765547
spaaaaaaam <samuel.ritz@stu.elps.us>
2025-12-08 12:50:46 -0500

    12/8/25

--- SamScripCompiler/src/Main.java
+++ SamScripCompiler/src/Main.java
@@ -1,14 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-
-
-
 
 public class Main {
 
-    static void parse_line(String line) {
+    ArrayList<ArrayList<String>> Command_groups = new ArrayList<ArrayList<String>>();
+
+    /***
+     *
+     * finds the length of the line and chops it up into a String[]
+     *
+     * @param line one line of code to be chopped up into an array
+     * @return String[] of chopped up line
+     */
+    static String[] parse_line(String line) {
 
         int numOfWords = 1;
         for (int i = 0; i < line.length(); i++) {
@@ -22,21 +29,25 @@ public class Main {
         char space = ' ';
         String word = "";
         String[] words = new String[numOfWords];
-        while (i < line.length()){
+        while (i < line.length()) {
             if (!(line.charAt(i) == space)) {
                 word = word + line.charAt(i);
             } else {
-                words[j] = word;
-                j += 1;
-            }
-        }
 
+                words[j] = word;
+                j ++;
+                word = "";
+            }
+            i += 1;
+        }
+        return(words);
     }
 
-
-    ArrayList<String> functions = new ArrayList<String>();
-
-    public static void main(String[] args) {
+    /***
+     * reads the file then chops its up into and array List
+     * @return ArrayList of String[]s that contain the line info
+     */
+    static ArrayList<String[]> get_file() {
 
         File file = new File("src/code.smsc");
 
@@ -47,13 +58,42 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+        ArrayList<String[]> parsed_Lines = new ArrayList<String[]>();
+
         while (sc.hasNextLine()){
             String currentLine = sc.nextLine();
             if (!currentLine.equals("")){
-
-                parse_line(currentLine);
+                parsed_Lines.add(parse_line(currentLine));
             }
 
+        }
+        return(parsed_Lines);
+    }
+
+
+    public static void main(String[] args){
+
+        System.out.println("compiling...");
+        ArrayList<String[]> file = get_file();
+
+        for (int i = 0; i < file.size(); i++){
+            String[] line = file.get(i);
+            boolean indented = false;
+
+            for (int j = 0; j < line.length; j++){
+                String word = line[j];
+
+                if (word.equals(" ")){ // if Starts with indentation
+                    indented = true;
+                    continue;
+                }
+                switch(word){
+                    case "Command_group":
+
+
+                }
+
+            }
         }
     }
 }
